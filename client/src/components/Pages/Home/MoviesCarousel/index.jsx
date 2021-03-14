da5a1ce2a06875b34d42ce7be96f768a4bf32f1f
Andre <vtavares.andre@gmail.com>
2021-03-14 13:47:21 -0300

    add courosel to home

---
+++ client/src/components/Pages/Home/MoviesCarousel/index.jsx
@@ -0,0 +1,61 @@
+import React, { useState, useEffect, useContext } from 'react'
+import { Link } from 'react-router-dom';
+import { Card, Container, Image, Row } from 'react-bootstrap';
+
+
+// https://www.npmjs.com/package/react-alice-carousel
+import AliceCarousel from 'react-alice-carousel';
+import "react-alice-carousel/lib/alice-carousel.css";
+
+const MoviesCarousel = ({ moviesCarousel, moviesCarouselName }) => {
+    const imagePath = 'https://image.tmdb.org/t/p/w500/'
+
+    const items = moviesCarousel.map((movie) => (
+        <Link key={movie.id} to={`/movies/details/${movie.id}`} >
+        <Card style={{ width: '16rem' }} className="bg-dark text-white" key={movie.id}>
+            <Card.Img src={imagePath + movie.poster_path} alt="Card image" />
+
+        </Card>
+        </Link>
+    ))
+
+    const numMovies = (moviesCarousel.length -4)
+    const [activeIndex, setActiveIndex] = useState(0);
+
+    const slidePrev = () => {
+        if(activeIndex > 0 && activeIndex <= numMovies )
+        setActiveIndex(activeIndex - 1)
+    }
+    const slideNext = () => {
+        if(activeIndex < numMovies )
+        setActiveIndex(activeIndex + 1)
+    }
+
+    const onSlideChanged = ({ item }) => setActiveIndex(item);
+
+    return (
+        <div className="mt-5">
+            <div className="m-3">
+
+                <h2>{moviesCarouselName}</h2>
+            </div>
+
+            <AliceCarousel disableButtonsControls 
+                mouseTracking
+                infinite
+                onSlideChanged={onSlideChanged}
+                activeIndex={activeIndex}
+                autoWidth  items={items} />
+                
+            {/* <div className="b-refs-buttons">
+                <button onClick={slidePrev}>Prev</button>
+                <button onClick={slideNext}>Next</button>
+                {activeIndex}
+                {numMovies}
+            </div> */}
+
+        </div>
+    )
+}
+
+export default MoviesCarousel
--- client/src/components/Pages/Home/index.jsx
+++ client/src/components/Pages/Home/index.jsx
@@ -4,24 +4,52 @@ import Axios from 'axios'
 import { Card, Container, Image, Row } from 'react-bootstrap';
 import Header from '../../shared/Header';
 import Banner from './Banner';
+import HomeCarousel from './MoviesCarousel';
 import { Link } from 'react-router-dom';
+import MoviesCarousel from './MoviesCarousel';
 
 const Home = () => {
   const tmdbKey = '16de3d175c4180739924271ad90578a1'
   // const fetchURL = `https://www.omdbapi.com/?s=${movieInputFromClick}&type=movie&page=1&apikey=8a2a252`
   const fetchtbdbURL = `https://api.themoviedb.org/3/movie/popular?api_key=16de3d175c4180739924271ad90578a1&language=en-US&page=1`
+  const topMoviesURL = `https://api.themoviedb.org/3/movie/top_rated?api_key=16de3d175c4180739924271ad90578a1&language=en-US&page=1`
+  const trendingMoviesURL = `https://api.themoviedb.org/3/trending/movie/week?api_key=16de3d175c4180739924271ad90578a1`
   const imagePath = 'https://image.tmdb.org/t/p/w500/'
 
 
   const [homeMovies, setHomeMovies] = useState([])
+  const [topMovies, setTopMovies] = useState([])
+  const [trendingMovies, setTrendingMovies] = useState([])
 
   useEffect(() => {
+    Axios.get(trendingMoviesURL)
+      .then(res => {
+        console.log(res)
+        console.log(res.data.results)
+        setTrendingMovies(res.data.results)
+
+      })
+      .catch((err) => {
+        if (err.response) {
+          console.log("respo")
+        } else if (err.request) {
+          console.log("request")
+          console.log(err)
+        } else {
+          console.log(err)
+        }
+      })
+    Axios.get(topMoviesURL)
+      .then(res => {
+        console.log(res.data.results)
+        setTopMovies(res.data.results)
+      })
+
     Axios.get(fetchtbdbURL)
       .then(res => {
         console.log(res)
         console.log(res.data.results)
         setHomeMovies(res.data.results)
-
       })
       .catch((err) => {
         if (err.response) {
@@ -42,16 +70,20 @@ const Home = () => {
 
       <Banner title='Discover Movies and Where to watch Them' subTitle='' img='' />
 
-      {/* <Container> */}
-          <h2>Fetured</h2>
-        <Row className='d-flex'>
+      <MoviesCarousel moviesCarousel={homeMovies}  moviesCarouselName={'Popular'}/>
+      <MoviesCarousel moviesCarousel={topMovies} moviesCarouselName={'Top Movies'}/>
+      <MoviesCarousel moviesCarousel={trendingMovies} moviesCarouselName={'Trending Movies '}/>
+
+
+      <h2>Fetured</h2>
+      <Row className='d-flex'>
         {homeMovies.map((movie) => (
 
-          <Link to={`/movies/details/${movie.id}`} >
-          
-          <Card style={{ width: '16rem' }} className="bg-dark text-white">
-          <Card.Img src={imagePath + movie.poster_path} alt="Card image" />
-          {/* <Card.ImgOverlay>
+          <Link key={movie.id} to={`/movies/details/${movie.id}`} >
+
+            <Card style={{ width: '16rem' }} className="bg-dark text-white">
+              <Card.Img src={imagePath + movie.poster_path} alt="Card image" />
+              {/* <Card.ImgOverlay>
             <Card.Title>{movie.title}</Card.Title>
             <Card.Text>
             This is a wider card with supporting text below as a natural lead-in to
@@ -59,12 +91,12 @@ const Home = () => {
             </Card.Text>
             <Card.Text>Last updated 3 mins ago</Card.Text>
           </Card.ImgOverlay> */}
-          
-          </Card>
+
+            </Card>
           </Link>
         ))}
-        </Row>
-     {/* </Container> */}
+      </Row>
+
 
 
 
