664642238a1b688650270e2fb5b06521c5946eb1
Andre <vtavares.andre@gmail.com>
2021-03-25 20:38:40 -0300

    clean app.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
-import Navigation from './components/shared/Navigation';
+
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Routes from './components/shared/Routes';
+
 import UserProvider from './components/Authentication/UserProvider';
-import NotificationProvider from './components/shared/Notifications';
+
 import GlobalStoreProvider from './components/shared/Globals';
-import Footer from './components/shared/Footer';
+
 import Layout from './components/shared/Layout';
 
 const App = () => {
